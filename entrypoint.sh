0f9067d6a915906e5ea38c9b971133867829cef7
Simon Viénot <simon.vienot@icloud.com>
2023-07-27 17:38:04 +0200

    Make URL of sourcify server configurable via runtime environment variable.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,14 @@
 # Start php fpm
 /etc/init.d/php7.4-fpm start
 
+# Patch index.html to insert config parameter in the global window object
+CONFIG_STRING='\
+    <script> \
+      window.configs = { \
+        "SERVER_URL":"'"${SERVER_URL}"'", \
+      } \
+    </script>'
+sed -i "s@<script></script>@${CONFIG_STRING}@" /redirects/index.html
+
 # Start nginx
 nginx -g "daemon off;"
