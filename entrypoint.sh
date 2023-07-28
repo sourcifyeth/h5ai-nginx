64d855a8cc1de35d382653660197c3ce2ed77024
Simon Viénot <simon.vienot@icloud.com>
2023-07-29 00:00:19 +0200

    Make patching of index.html work on linux.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,13 +7,7 @@
 /etc/init.d/php7.4-fpm start
 
 # Patch index.html to insert config parameter in the global window object
-CONFIG_STRING='\
-    <script> \
-      window.configs = { \
-        "SERVER_URL":"'"${SERVER_URL}"'", \
-      } \
-    </script>'
-sed -i "s@<script></script>@${CONFIG_STRING}@" /redirects/index.html
+sed -i "s@<script></script>@<script>window.configs={SERVER_URL:\"${SERVER_URL}\"}</script>@" /redirects/index.html
 
 # Start nginx
 nginx -g "daemon off;"
